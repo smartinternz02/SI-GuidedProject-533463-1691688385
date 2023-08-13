98954a2cbeb26cda69b298edf321ba4f1c091d47
Prashant Lakkadwala <lakkadwala21@gmail.com>
2023-08-13 10:46:59 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -247,7 +247,7 @@ def marksassign(stdname):
 
     cos = ibm_boto3.client("s3", ibm_api_key_id=COS_API_KEY_ID, ibm_service_instance_id=COS_INSTANCE_CRN, config=Config(signature_version="oauth"), endpoint_url=COS_ENDPOINT)
 
-    prefix = "prashantstudent/" + stdname
+    prefix = stdname
 
     response = cos.list_objects(Bucket=BUCKET_NAME, Prefix=prefix)
     objects = response.get('Contents', [])
